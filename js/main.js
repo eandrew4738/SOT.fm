4153cc4319c5eea07753576e8fbd1beec72fbdcd
Vivek_sam <viveksam@gmail.com>
2018-08-07 23:04:41 +0530

    Updated

--- js/main.js
+++ js/main.js
@@ -20,8 +20,6 @@ $(function functionName() {
      {
       var text = document.getElementById("text");
       var displayText = document.getElementById("displayText");
-
-      console.log(endValue);
       
       if (endValue < 40) {
         text.style.display = "none",
@@ -72,23 +70,6 @@ $(function functionName() {
   });
 
 });
-
-function startTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('timeText').innerHTML =
-    h + ":" + m + ":" + s;
-  var t = setTimeout(startTime, 500);
-}
-
-function checkTime(i) {
-  if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
-  return i;
-}
 
 $( document ).ready(function() {
   $('.radioco_artwork').bind('DOMNodeInserted', function(event) {
